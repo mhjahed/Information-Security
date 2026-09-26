4672a60a20a8350adc89c521b7c86cf3869ec7ea
INVISIBLE- MH <146859747+mhjahed@users.noreply.github.com>
2026-09-26 13:02:58 +0600

    Create script.js
    Initial Commit of Script file for dynamic practice

---
+++ JS Practice/script.js
@@ -0,0 +1,75 @@
+//In thsi script this is an example for seperated file for JS and how to connect to HTML file
+//Use semicolone so that you can seperate the codes
+//don't use same strings in a same log or code
+//Use alternate strings in a same output
+//comment can ignore a single line
+//To comment a whole code just use /* YOUR CODE*/
+
+
+
+
+//console print in console p-1
+console.log("JS file connected");
+//console print in console p-2
+console.log("I am line 1");
+//console print in console p-3
+console.log("I am line 2");
+        //window alet 
+        window.alert("I am watching you");
+        //alert alternate
+        //alert("I am good")
+        //document get and write
+       document.getElementById("root").innerHTML = "I am RTX 3090Ti";
+       
+       /*document write without ID, class, element
+       document.write("Hello SIR");*/
+
+       //document write by ID 2
+       document.getElementById("idH2").innerHTML = "This is from H2 ID";
+       
+       //document write by class 
+       document.getElementById("idH3").innerHTML = "This is from H2 ID";
+       
+       /*document number without string
+       document.write(89.76);*/
+
+
+       /*Get Input from USER
+       prompt("Enter Name");*/
+
+       //var to declare the variable to say to JS that this is a variable
+       var x;
+       //declare what will be the variable
+       x = prompt("ENTER NAME");
+       //print the value of the variable as you want(console, document)
+       console.log(x);
+       document.write(x);
+
+       //store values in variable using var like number text or symbols
+       var dt;
+       dt = 43;
+       //Or in a single line
+       var dx =5;
+       console.log(dx);
+       console.log(dt);
+       //declare var in let
+       let dy = 90;
+       console.log(dy);
+
+       //calling latest value
+       var dk;
+       dk = 45; 
+       console.log(dk);
+       dk = 35;
+
+       //calling different values of a single variables
+       var dr;
+       dr = 10;
+       console.log(dr);
+       dr = "jahed";
+       console.log(dr);
+
+
+    
+
+
